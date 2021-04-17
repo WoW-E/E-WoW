eb7232a3fae451bd5640c2c092e781e9c7b1b8f7
dhruvjadhav <dhruvjadhav225@gmail.com>
2021-04-17 16:04:47 +0530

    Removed keep_alive.py. Modified requirements.txt.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import random
 
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
-from keep_alive import keep_alive
+load_dotenv('.env')
 
 
 def get_prefix(client, message):
@@ -178,7 +179,5 @@ async def dm(ctx, user: discord.Member = None, *, message=None):
     else:
         await ctx.send('Invalid Command?')
 
-
-keep_alive()
 
 client.run(os.getenv('TOKEN'))
